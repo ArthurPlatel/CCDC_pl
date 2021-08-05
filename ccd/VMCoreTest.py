3878f262f31967e6cdc2de36ff16b67b64dab6d3
ArthurPlatel <arthur.platel@planet.com>
2021-08-05 15:21:58 -0700

    Core test update

--- ccd/VMCoreTest.py
+++ ccd/VMCoreTest.py
@@ -6,7 +6,7 @@ def main(parent_dir):
     timelist=[]
     for k in range(4):
         now1=time.time()
-        imageCCD(parent_dir,"na",size=k+1,test=True,print=False)
+        imageCCD(parent_dir,"na",size=k+1,test=True,write=False)
         thread_1=time.time()-now1
         timelist.append(thread_1)
     for k in range(4):
