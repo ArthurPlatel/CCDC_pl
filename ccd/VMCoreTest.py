59e446b1174d460e75784f28b0b54b8470ec50c1
ArthurPlatel <arthur.platel@planet.com>
2021-08-05 16:42:33 -0700

    Test Update

--- ccd/VMCoreTest.py
+++ ccd/VMCoreTest.py
@@ -9,10 +9,10 @@ def main(parent_dir):
         imageCCD(parent_dir,"na",k+1,test=True,write=False)
         thread_1=time.time()-now1
         timelist.append(thread_1)
-    for k in range(4):
-        print("CCD code using {} threads processed 20 rows in {}".format(k+1,timelist[k]))
+    for k in range(8):
+        print("CCD code using {} threads processed 100 pixels in {}".format(k+1,timelist[k]))
 
-# if __name__ == '__main__':
-#     parent_dir='/Users/arthur.platel/Desktop/Fusion_Images/CZU_FireV2'
-#     main(parent_dir)
+if __name__ == '__main__':
+    parent_dir='/Users/arthur.platel/Desktop/Fusion_Images/CZU_FireV2'
+    main(parent_dir)
 
