10db70254dd5d957732f2360c3efe0ff0ffe9f2b
ArthurPlatel <arthur.platel@planet.com>
2021-09-14 03:11:41 -0700

    added argparse

--- jobsScripts/ReadResults.py
+++ jobsScripts/ReadResults.py
@@ -5,45 +5,63 @@ import os
 import glob
 from ccd import detect
 import json
+import argparse
+import time
 
 def main():
-    parent_dir='/Users/arthur.platel/Desktop/PF-SR/'
-    csv_dir= parent_dir+'/pixelValues'
+    
+    ###Argparse settings
+    parser = argparse.ArgumentParser(description="CCDC", allow_abbrev=False, add_help=False)
+    parser.add_argument("-d",action="store", metavar="directory", type=str, help="Directory of image stack")
+    args = parser.parse_args()
+    
+    #variables
+    parent_dir='/Users/arthur.platel/Desktop/PF-SR'
+    csv_dir= str(parent_dir)+'/pixelValues'
     files = glob.glob(os.path.join(csv_dir, '*m.csv'))
     imageData=glob.glob(os.path.join(csv_dir, 'imageData*.csv'))[0]
+    
+    #run code on each data CSV file in directory/can change to single file
     #for file in files:
     readResults(files[0], imageData,csv_dir)
 
 def readResults(file, imageData,csv_dir):
-    print(file[0:2])
+
+    #read csv file and imageData CSV
     df=pd.read_csv(file,header=0)
     df2=pd.read_csv(imageData, header=None)
+   
     ####Variables######
     pixel_count=int(df2[1][0])
     shape=eval(df2[2][0])
-    print(shape[1]*shape[2])
-    print(pixel_count)
     dates =eval(df2[4][0])
     params=dfs['params']
     qas=[1]*len(dates)
-    print(df.keys()[2:7])
-    for pix in range(5):
+    pixel1=eval(df['pixel'][0])
+    num=(((pixel1[0]*shape[1])+pixel1[1]))//pixel_count
+    
+    #determine CSV number to write
+    with open(str(csv_dir)+'/'+str(num)+'_Results.json', 'w') as outfile:
+            json.dump([],outfile)
+    
+    ####Calculate CCD for each pixel
+    print("calulating CCD Results")
+    for pix in range(pixel_count):
+        start=time.time()
         pixel=eval(df['pixel'][pix])
-        num=(((pixel[0]*shape[1])+pixel[1]))//pixel_count
         data =[[eval(df[band][pix])]for band in df.keys()[2:7]]
         blues,greens,reds,nirs,ndvis=data
         results=detect(dates,blues[0],greens[0],reds[0],nirs[0],ndvis[0],qas,params)
-        #write results to json
+        ##write results to json
         ccdResults={str(pixel):results}
         with open(str(csv_dir)+'/'+str(num)+'_Results.json', 'r') as outfile:
             dat=json.load(outfile) 
             dat.append(ccdResults)
         with open(str(csv_dir)+'/'+str(num)+'_Results.json', "w") as file:
             json.dump(dat, file)
+        end=time.time()-start
+        print('processed pixel {} in {}'.format(pixel,end))
 
-   
-def getImageData(csv_dir):
-    allFiles = glob.glob(os.path.join(csv_dir, '*.mcsv'))
 
 if __name__ == '__main__':
     main()
